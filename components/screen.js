0cb97a1c7b54c35437f312ceec2f56e980f030af
Christopher Bradford <chris.bradford@leasequery.com>
2021-04-28 01:41:10 -0400

    done for the night

--- components/screen.js
+++ components/screen.js
@@ -1,11 +1,10 @@
-import styles from "./screen.module.css"
+import styles from "./screen.module.css";
+import { Howl, Howler } from "howler";
 
 export default function Sreen() {
   return (
     <div className={styles.screen}>
-      <div className={styles.crt}>
-        {/* Savoy Sounds */}
-      </div>
+      <div className={styles.crt}>{/* Savoy Sounds */}</div>
     </div>
   );
 }
