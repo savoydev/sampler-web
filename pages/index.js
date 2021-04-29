5e8ae52dd6c8612ffb0233150dead771482c0e93
Christopher Bradford <chris.bradford@leasequery.com>
2021-04-29 08:21:25 -0400

    removed red borders, changed title, added favicon

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>MPC Sampler</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
